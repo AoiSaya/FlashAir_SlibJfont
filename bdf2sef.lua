b3cb2343b7e560a126b89083cc03e284c638132a
AoiSaya <42811276+AoiSaya@users.noreply.github.com>
2019-05-14 01:25:11 +0900

    for proportional font

--- bdf2sef.lua
+++ bdf2sef.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------
 -- bdf2sef.lua for FlashAir W4.00.03
 -- *.SEF(Saya Euc Font format) is original format based on EUC
--- 2018/11/28 rev.0.2
+-- 2019/05/13 rev.0.3
 -------------------------------------------------------
 function chkBreak(n)
 	sleep(n or 0)
@@ -31,17 +31,15 @@ function getData(sfh,format,base)
 	return table.unpack(ret)
 end
 
-function idiv(num,dev)
-	return (num-num%dev)/dev,num%dev
-end
-
-function convBdf2Sef(srcFname,dstFname,byte)
+function convBdf2Bin(srcFname,dstFname,byte)
 	local sfh, dfh
-	local bw, bh, ox, oy, ndeg, sz, blank, chars, charL
+	local bbw, bbh, box, boy, ndeg, sz, blank, chars, charL
 	local char, char_next, char_base, buf, bin, d, u
 	local dwidth, fbw, fbh, fox, foy
 	local head = "SEF"
-	local rev = 2
+	local rev = 3
+	local ba = bit32.band
+	local bx = bit32.extract
 
 	sfh = io.open(srcFname,"r")
 	if not sfh then
@@ -53,20 +51,16 @@ function convBdf2Sef(srcFname,dstFname,byte)
 		return nil, "Can't open "..'"'..dstFname..'".'
 	end
 
-	bw, bh, ox, oy = getData(sfh, "^FONTBOUNDINGBOX (%w+) (%w+) (-?%w+) (-?%w+)",10)
-	chars  = getData(sfh, "^CHARS (%w+)",10)
-	bm	   = idiv(bw+7,8) * 8
-	ndeg   = idiv(bh+3,4)
-	sz	   = ndeg * bw + 3
+	bbw, bbh, box, boy = getData(sfh, "^FONTBOUNDINGBOX%s+(%w+)%s+(%w+)%s+(-?%w+)%s+(-?%w+)",10)
+	chars  = getData(sfh, "^CHARS%s+(%w+)",10)
+	ndeg   = ba(bbh+3,0xFFFFFFF0)
+	sz	   = ndeg * bbw + 3
 	blank  = string.format("%02X",bw)..string.rep("0", sz-3) .. "\n"
-	if rev==0 then
-		sz = sz + 4
-	end
-	char_base = (byte==1) and 0x20 or 0x2121+0x8080
+	char_base = (byte==1) and 0x00 or 0x2121+0x8080
 	char_next = char_base
 	header_size = 64
 
-	buf = string.format(head.."%3X%2X%2X%2X%3X%4X\n",header_size,rev,bw,bh,sz,char_base)
+	buf = string.format(head.."%3X%2X%2X%2X%3X%4X\n",header_size,rev,bbw,bbh,sz,char_base)
 	dfh:write(buf)
 	buf = string.rep(" ", header_size-#buf-1).."\n"
 	dfh:write(buf)
@@ -75,17 +69,14 @@ function convBdf2Sef(srcFname,dstFname,byte)
 --	for i=1, 100 do
 		chkBreak()
 		collectgarbage()
-		char  = getData(sfh, "^STARTCHAR (%w+)",16)
+		char  = getData(sfh, "^STARTCHAR%s+(%w+)",16)
 		if byte==2 then
 			char = char+0x8080
 		end
 		if char~=char_next then
 			for j=char_next, char-1 do
-				charL = j % 256
+				charL = ba(j,0x00FF)
 				if byte==1 or (byte==2 and 0xA1<=charL and charL<=0xFE) then
-					if rev==0 then
-						dfh:write(string.format("%4X",j))
-					end
 					dfh:write(blank)
 				end
 			end
@@ -93,24 +84,23 @@ function convBdf2Sef(srcFname,dstFname,byte)
 		char_next = char+1
 
 		dwidth	= getData(sfh, "^DWIDTH (%w+)",10)
-		fbw, fbh, fox, foy = getData(sfh, "^BBX (%w+) (%w+) (-?%w+) (-?%w+)",10)
+		fbw, fbh, fox, foy = getData(sfh, "^BBX%s+(%w+)%s+(%w+)%s+(-?%w+)%s+(-?%w+)",10)
 		getData(sfh, "^BITMAP")
 		bin = {}
-		for k=1, bw do
+		for k=1, bbw do
 			bin[k] = 0
 		end
+		u = 2^(fbh+foy-(bbh+boy))
+		bm = ba(fbw+7,0xFFFFFF00)
 		for j= 1, bh do
 			d = getData(sfh, "(%w+)",16)
-			u = 2^(j-1)
-			for k=1, bw do
-				bin[k]=bin[k]+bit32.extract(d,bm-k)*u
+			for k=1, fbw do
+				bin[k+fox] = bin[k+fox]+bx(d,bm-k)*u
 			end
+			u = u+u
 		end
-		if rev==0 then
-			dfh:write(string.format("%4X",char))
-		end
-		dfh:write(string.format("%02X",fbw))
-		for k=1, bw do
+		dfh:write(string.format("%02X",dwidth))
+		for k=1, bbw do
 			dfh:write(string.format("%0"..ndeg.."X",bin[k]))
 		end
 		dfh:write("\n")
@@ -122,38 +112,34 @@ end
 
 
 local myDir  = script_path()
+
 --[[
 local srcFname = myDir .. "misaki_4x8_iso8859.bdf"
 local dstFname = myDir .. "misaki_4x8_iso8859.sef"
-convBdf2Sef(srcFname,dstFname,1)
---]]
---[[
+convBdf2Bin(srcFname,dstFname,1)
 local srcFname = myDir .. "misaki_4x8_jisx0201.bdf"
 local dstFname = myDir .. "misaki_4x8_jisx0201.sef"
-convBdf2Sef(srcFname,dstFname,1)
---]]
---[[
+convBdf2Bin(srcFname,dstFname,1)
 local srcFname = myDir .. "misaki_gothic.bdf"
 local dstFname = myDir .. "misaki_gothic.sef"
-convBdf2Sef(srcFname,dstFname,2)
---]]
---[[
-local srcFname = myDir .. "misaki_mincho.bdf"
+convBdf2Bin(srcFname,dstFname,2)
+ocal srcFname = myDir .. "misaki_mincho.bdf"
 local dstFname = myDir .. "misaki_mincho.sef"
-convBdf2Sef(srcFname,dstFname,2)
---]]
----[[
+convBdf2Bin(srcFname,dstFname,2)
 local srcFname = myDir .. "k6x8.bdf"
 local dstFname = myDir .. "k6x8.sef"
-convBdf2Sef(srcFname,dstFname,2)
---]]
---[[
+convBdf2Bin(srcFname,dstFname,2)
 local srcFname = myDir .. "k6x10.bdf"
 local dstFname = myDir .. "k6x10.sef"
-convBdf2Sef(srcFname,dstFname,1)
---]]
---[[
+convBdf2Bin(srcFname,dstFname,1)
 local srcFname = myDir .. "k12x10.bdf"
 local dstFname = myDir .. "k12x10.sef"
-convBdf2Sef(srcFname,dstFname,2)
+convBdf2Bin(srcFname,dstFname,2)
 --]]
+local srcFname = myDir .. "3x8.bdf"
+local dstFname = myDir .. "3x8.sef"
+convBdf2Bin(srcFname,dstFname,1)
+
+local srcFname = myDir .. "shnmk12p.bdf"
+local dstFname = myDir .. "shnmk12p.sef"
+convBdf2Bin(srcFname,dstFname,2)
